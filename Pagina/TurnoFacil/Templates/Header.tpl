172af29a454832a6fffc5596af1a41bbb850f1c6
Viviana Ayelen Diaz Bailey <ayediazbailey@gmail.com>
2022-05-29 19:37:10 -0300

    Agregado el Header

--- Pagina/TurnoFacil/Templates/Header.tpl
+++ Pagina/TurnoFacil/Templates/Header.tpl
@@ -8,3 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
 </head>
 <body>
+
+<header>
+    <div class="logo">
+        <img id="logo-shelby" src="../TurnoFacil/Images/logo.png" alt="logo" srcset="">
+    </div>
+</header>
---
+++ Pagina/TurnoFacil/templates_c/5b50c87908bf5559f7e4c7a18e4dfea78ccc738a_0.file.Footer.tpl.php
@@ -0,0 +1,36 @@
+<?php
+/* Smarty version 4.1.0, created on 2022-05-29 23:51:11
+  from 'C:\xampp\htdocs\Meto\MET-GRUPO-4-2022\Pagina\TurnoFacil\templates\Footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.1.0',
+  'unifunc' => 'content_6293eacfb15a18_18503962',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '5b50c87908bf5559f7e4c7a18e4dfea78ccc738a' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\Meto\\MET-GRUPO-4-2022\\Pagina\\TurnoFacil\\templates\\Footer.tpl',
+      1 => 1653854779,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6293eacfb15a18_18503962 (Smarty_Internal_Template $_smarty_tpl) {
+?>    </main>
+    <footer>
+    </footer>
+
+
+
+    <?php echo '<script'; ?>
+ src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+</body>
+
+</html><?php }
+}
