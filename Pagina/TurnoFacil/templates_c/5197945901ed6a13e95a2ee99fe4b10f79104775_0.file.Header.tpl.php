ffb131c18e818ebcb5afa93b2e4eb623f90c32d5
Viviana Ayelen Diaz Bailey <ayediazbailey@gmail.com>
2022-05-29 20:01:03 -0300

    Agregada hoja de estilos

--- Pagina/TurnoFacil/templates_c/5197945901ed6a13e95a2ee99fe4b10f79104775_0.file.Header.tpl.php
+++ Pagina/TurnoFacil/templates_c/5197945901ed6a13e95a2ee99fe4b10f79104775_0.file.Header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.1.0, created on 2022-05-30 00:17:54
+/* Smarty version 4.1.0, created on 2022-05-30 00:59:40
   from 'C:\xampp\htdocs\Meto\MET-GRUPO-4-2022\Pagina\TurnoFacil\templates\Header.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.1.0',
-  'unifunc' => 'content_6293f1120a5272_71600124',
+  'unifunc' => 'content_6293fadc808c01_85680604',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '5197945901ed6a13e95a2ee99fe4b10f79104775' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Meto\\MET-GRUPO-4-2022\\Pagina\\TurnoFacil\\templates\\Header.tpl',
-      1 => 1653862655,
+      1 => 1653865177,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_6293f1120a5272_71600124 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6293fadc808c01_85680604 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html>
 <head>
@@ -30,6 +30,7 @@ function content_6293f1120a5272_71600124 (Smarty_Internal_Template $_smarty_tpl)
 </title>
     <!--Bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
 
