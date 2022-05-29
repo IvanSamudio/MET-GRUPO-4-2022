3505254e055113b55a576a4ddf22788639c4585b
beltran peña <beltran.pe99@gmail.com>
2022-05-29 18:46:44 -0300

    creado MVC de medico

---
+++ Pagina/Model/MedicoModel.php
@@ -0,0 +1,51 @@
+<?php
+
+class MedicoModel{
+    
+    function __construct()
+    {
+      $this->db = new PDO('mysql:host=localhost;'
+      .'dbname=;charset=utf8'
+      , 'root', '');
+    }
+
+    function GetMedicos(){
+        $sentencia = $this->db->prepare("SELECT * FROM medico");
+        $sentencia->execute();
+        return $sentencia->fetchAll(PDO::FETCH_ASSOC);
+      }
+
+    function GetMedicosPorEspecialidad($especialidad){
+        $sentencia = $this->db->prepare("SELECT * FROM medico WHERE especialidad = ? ");
+        $sentencia->execute($especialidad);
+        return $sentencia->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function GetMedicosPorObraSocial($obraSocial){
+        $sentencia = $this->db->prepare("SELECT * FROM medico WHERE obraSocial = ? ");
+        $sentencia->execute($obraSocial);
+        return $sentencia->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function GetMedicosPorEspecialidadYObra($especialidad, $obraSocial){
+        $sentencia = $this->db->prepare("SELECT * FROM medico WHERE especialidad = ? AND obraSocial = ? ");
+        $sentencia->execute($especialidad, $obraSocial);
+        return $sentencia->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function UpdateMedico($especialidad, $horario, $contrasenia, $secretaria, $obraSocial, $nroMatricula){
+        $sentencia = $this->db->prepare("UPDATE medico SET especialidad = ?, horario = ?, contrasenia = ?, secretaria = ?, obraSocial=? WHERE nro_matricula = ?");
+        $sentencia->execute($especialidad, $horario, $contrasenia,$secretaria,$obraSocial,$nroMatricula);
+        return $sentencia->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    function DeleteMedico($nroMatricula){
+        $sentencia = $this->db->prepare("DELETE FROM medico WHERE nro_matricula = ?");
+        $sentencia->execute($nroMatricula);
+    }
+
+    function InsertMedico($nroMatricula, $especialidad, $horario,$contrasenia,$obraSocial)   {
+        $sentencia = $this->db->prepare("INSERT INTO medico VALUES(?,?,?,?,NULL,?)");
+        $sentencia->execute([$nroMatricula,$especialidad,$horario,$contrasenia,$obraSocial]);
+    }  
+}
