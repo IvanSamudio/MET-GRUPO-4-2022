d3a0e65df9d4f448b76025c77ce0c96ca9c55fb6
IvanSamudio <ivansamudio07@gmail.com>
2022-05-29 23:28:13 -0300

    En progreso

--- Pagina/TurnoFacil/templates_c/f872e308421a30135307161b7c4ec6c18991d24f_0.file.Header.tpl.php
+++ Pagina/TurnoFacil/templates_c/f872e308421a30135307161b7c4ec6c18991d24f_0.file.Header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.1.0, created on 2022-05-18 23:22:30
+/* Smarty version 4.1.0, created on 2022-05-30 03:44:07
   from 'C:\xampp\htdocs\proyectos\Proyecto\MET-GRUPO-4-2022\Pagina\TurnoFacil\templates\Header.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.1.0',
-  'unifunc' => 'content_628563967aa0b1_98486212',
+  'unifunc' => 'content_6294216713bfe6_52127515',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'f872e308421a30135307161b7c4ec6c18991d24f' => 
     array (
       0 => 'C:\\xampp\\htdocs\\proyectos\\Proyecto\\MET-GRUPO-4-2022\\Pagina\\TurnoFacil\\templates\\Header.tpl',
-      1 => 1652906747,
+      1 => 1653875033,
       2 => 'file',
     ),
   ),
@@ -20,15 +20,23 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_628563967aa0b1_98486212 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6294216713bfe6_52127515 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title><?php echo $_smarty_tpl->tpl_vars['Titulo']->value;?>
 </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilos.css">
 </head>
-<body><?php }
+<body>
+
+<header>
+    <div class="logo">
+        <img id="logo-shelby" src="../TurnoFacil/Images/logo.png" alt="logo" srcset="" width="100px" height="100px">
+    </div>
+</header><?php }
 }
